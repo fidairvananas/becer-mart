ec347f29fdc578f4eb118239697e921ff90bac5a
fida irvan anas <fidairvan@gmail.com>
2025-08-17 14:27:31 +0700

    init products

---
+++ backend/src/main/java/com/becermart/BecermartApplication.java
@@ -0,0 +1,13 @@
+package com.becermart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecermartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecermartApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/product/becermart/BecermartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.becermart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecermartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
