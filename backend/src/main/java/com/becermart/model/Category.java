9d4633fca20c86779b2d00b2f1dcc839cba41c80
fida irvan anas <fidairvan@gmail.com>
2025-08-17 14:21:04 +0700

    init products

---
+++ backend/src/main/java/com/becermart/model/Category.java
@@ -0,0 +1,35 @@
+package com.becermart.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "name")
+        })
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @org.hibernate.annotations.GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
---
+++ backend/src/main/java/com/becermart/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.becermart.repository;
+
+import com.becermart.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface CategoryRepository extends JpaRepository<Category, UUID> { }
